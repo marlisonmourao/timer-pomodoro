19183ed7e016cd319dacb8d6d4a8f9466f7a8e93
marlisonmourao <bentesmourao@gmail.com>
2023-01-28 20:55:30 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/theme/default"
+
+import { Button } from "./components/Button";
+
+export function App() {
+  return(
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger"/>
+    </ThemeProvider>
+  )
+}
---
+++ src/components/Button/styles.ts
@@ -0,0 +1,23 @@
+import { defaultTheme } from './../../styles/theme/default';
+import styled, { css } from "styled-components";
+
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success' 
+
+interface ButtonContainerProps {
+  variant?: ButtonVariant
+}
+
+const buttonVariants = {
+  primary: 'purple',
+  secondary: 'orange',
+  danger: 'red',
+  success: 'green'
+}
+
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+  width: 100px;
+  height: 40px;
+
+  background-color: ${({ theme }) => theme.primary};
+  color: ${({ theme }) => theme.white}
+`
